c3db6a5671905aefbefdedaa522bfa5f963c430f
c-ostic <60513574+c-ostic@users.noreply.github.com>
2021-11-06 22:08:47 -0400

    Added MMR
    Eventually ability for CPU to read and write from memory
    Ability to flash memory with a program

--- src/System.ts
+++ src/System.ts
@@ -3,6 +3,7 @@ import { Clock } from "./hardware/Clock";
 import { Cpu } from "./hardware/Cpu";
 import { Hardware } from "./hardware/Hardware";
 import { Memory } from "./hardware/Memory";
+import { MMU } from "./hardware/MMU";
 
 
 /*
@@ -21,6 +22,7 @@ export class System extends Hardware
 {
     private _CPU : Cpu = null;
     private _Memory : Memory = null;
+    private _MMU : MMU = null;
     private _Clock : Clock = null;
     
     public running: boolean = false;
@@ -31,6 +33,7 @@ export class System extends Hardware
 
         this._CPU = new Cpu(true);
         this._Memory = new Memory(true);
+        this._MMU = new MMU(true, this._Memory);
         this._Clock = new Clock(true, CLOCK_INTERVAL);
         
         /*
@@ -40,6 +43,8 @@ export class System extends Hardware
          */
 
         this.startSystem();
+
+        this._MMU.memoryDump(0x0000, 0x000F);
     }
 
     public startSystem(): boolean 
@@ -47,12 +52,29 @@ export class System extends Hardware
         this._Clock.register(this._CPU);
         this._Clock.register(this._Memory);
 
+        this.flashProgram();
+
         return true;
     }
 
     public stopSystem(): boolean 
     {
         return false;
+    }
+
+    public flashProgram() : void
+    {
+        this._MMU.writeImmediate(0x0000, 0xA9);
+        this._MMU.writeImmediate(0x0001, 0x0D);
+        this._MMU.writeImmediate(0x0002, 0xA9);
+        this._MMU.writeImmediate(0x0003, 0x1D);
+        this._MMU.writeImmediate(0x0004, 0xA9);
+        this._MMU.writeImmediate(0x0005, 0x2D);
+        this._MMU.writeImmediate(0x0006, 0xA9);
+        this._MMU.writeImmediate(0x0007, 0x3F);
+        this._MMU.writeImmediate(0x0008, 0xA9);
+        this._MMU.writeImmediate(0x0009, 0xFF);
+        this._MMU.writeImmediate(0x000A, 0x00);
     }
 }
 
---
+++ src/hardware/MMU.ts
@@ -0,0 +1,93 @@
+import { Hardware } from "./Hardware";
+import { Memory } from "./Memory";
+
+const ADD_SPACE_FMT_LEN : number = 4;
+const WORD_FMT_LEN : number = 2;
+
+export class MMU extends Hardware
+{
+    private _RAM : Memory;
+
+    constructor(debug : boolean, ram : Memory)
+    {
+        super(0, "MMU", debug);
+        this._RAM = ram;
+    }
+
+    /*
+    Sets the MAR of memory depending on what the CPU sends (full address, high order byte, low order byte)
+    address - the address to be set to the MAR
+    isPartial - whether or not this is a full address or a high/low order byte; default false
+    isHighOrder - whether or not this is a high or low order byte; default false
+    */
+    public setAddressByte(address : number, isPartial : boolean = false, isHighOrder : boolean = false) : void
+    {
+        if(isPartial)
+        {
+            var currMar : number = this._RAM.getMAR();
+
+            if(isHighOrder)
+            {
+                address = address << 8; //left shift byte 8 bits to correctly represent the high order
+                currMar = currMar & 0x00FF; //clear the high order byte of the current mar
+            }
+            else
+            {
+                currMar = currMar & 0xFF00; //clear the low order byte of the current mar
+            }
+
+            address = address | currMar; //add the shifted byte to half-cleared mar
+        }
+
+        //if there was no partial, then this just sets the mar to the full address
+        this._RAM.setMAR(address);
+    }
+
+    /*
+    Reads from memory with whatever address that was set with setAddressByte()
+    returns the data from memory at that address
+    */
+    public read() : number
+    {
+        this._RAM.read()
+        return this._RAM.getMDR();
+    }
+
+    /*
+    Writes some data into the address that was set with setAddressByte()
+    */
+    public write(data : number) : void
+    {
+        this._RAM.setMDR(data);
+        this._RAM.write();
+    }
+
+    /*
+    Immediately writes some data into a given address
+    */
+    public writeImmediate(address : number, data : number)
+    {
+        this.setAddressByte(address);
+        this.write(data);
+    }
+
+    /*
+    Logs a section of memory between two addresses
+    fromAddress - the first address to log
+    toAddress - the last address to log
+    */
+    public memoryDump(fromAddress : number, toAddress : number)
+    {
+        this.log("Memory Dump: Debug");
+        this.log("------------------------------------");
+
+        for(var i : number = fromAddress;i <= toAddress;i++)
+        {
+            this.setAddressByte(i);
+            this.log("Addr " + this.hexLog(i, ADD_SPACE_FMT_LEN) + "  |  " + this.hexLog(this.read(), WORD_FMT_LEN) + "");
+        }
+
+        this.log("------------------------------------");
+        this.log("Memory Dump: Complete");
+    }
+}
